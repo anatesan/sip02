29f5a745af897ecf8b152a578300add2617f37fa
Ashok Natesan <anatesan@walmartlabs.com>
2015-08-01 16:47:12 -0700

    sip02 username rather than sip
    fixed typo in schema.sql

--- src/main/sql/schema.sql
+++ src/main/sql/schema.sql
@@ -5,7 +5,7 @@ create table contact (
     first_name varchar(40) not null,
     mi char(1),
     email varchar(80),
-    date_created timestamp default 0,
-    date_modified timestamp default current_timestamp on update current_timestamp,
+    date_createdtimestamp default 0,
+    date_modified  timestamp default current_timestamp on update current_timestamp,
     unique index contact_idx1 (last_name, first_name, mi)
 ) engine = InnoDB;
